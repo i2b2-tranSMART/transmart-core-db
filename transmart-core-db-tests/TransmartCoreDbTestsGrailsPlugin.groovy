a95c0514c1ce034596800f18869be91e6d889918
Burt Beckwith <burt@burtbeckwith.com>
2018-03-02 20:09:01 -0500

    cleanup, version updates

--- transmart-core-db-tests/TransmartCoreDbTestsGrailsPlugin.groovy
+++ transmart-core-db-tests/TransmartCoreDbTestsGrailsPlugin.groovy
@@ -20,28 +20,24 @@
 import org.transmartproject.db.test.H2Views
 
 class TransmartCoreDbTestsGrailsPlugin {
-    def version = "16.2-SNAPSHOT"
-    def grailsVersion = "2.2 > *"
+	def version = '16.2'
+	def grailsVersion = '2.3 > *'
+	def title = 'Transmart Core Db Tests Plugin'
+	def author = 'Transmart Foundation'
+	def authorEmail = 'admin@transmartproject.org'
+	def description = 'Reuses logic for populating db with test data; also contains tests for core-db project to prevent circular plugin dependencies'
+	def documentation = 'http://transmartproject.org'
+	def license = 'GPL3'
+	def organization = [name: 'TODO', url: 'TODO']
+	def developers = [
+		[name: 'Ruslan Forostianov', email: 'ruslan@thehyve.nl'],
+		[name: 'Peter Kok', email: 'peter@thehyve.nl'],
+		[name: 'Burt Beckwith', email: 'burt_beckwith@hms.harvard.edu']
+	]
+	def issueManagement = [system: 'TODO', url: 'TODO']
+	def scm = [url: 'https://github.com/tranSMART-Foundation/transmart-core-db']
 
-    def title = "Transmart Core Db Tests Plugin"
-    def author = "Transmart Foundation"
-    def authorEmail = "admin@transmartproject.org"
-    def description = '''\
-        The aim of this plugin is to reuse logic for populating db with test data.
-        It also contains tests for core-db project to prevent circular
-        plugin dependencies, which grails does not resolve.
-    '''
-
-    def documentation = "http://transmartproject.org"
-
-    def scm = [ url: "https://fisheye.ctmmtrait.nl/browse/transmart_core_db" ]
-
-    def developers = [
-            [ name: "Ruslan Forostianov", email: "ruslan@thehyve.nl" ],
-            [ name: "Peter Kok", email: "peter@thehyve.nl" ]
-    ]
-
-    def doWithSpring = {
-        h2Views(H2Views)
-    }
+	def doWithSpring = {
+		h2Views(H2Views)
+	}
 }
