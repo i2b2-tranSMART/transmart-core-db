ee2bf91b7365a9dfb3b5233fc984bef1065fb5c1
Burt Beckwith <burt@burtbeckwith.com>
2018-03-02 19:47:11 -0500

    cleanup, version updates

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,63 +1,28 @@
-grails.project.class.dir = "target/classes"
-grails.project.test.class.dir = "target/test-classes"
-grails.project.test.reports.dir = "target/test-reports"
-
-def defaultVMSettings = [
-        maxMemory: 768,
-        minMemory: 64,
-        debug:     false,
-        maxPerm:   256
-]
-
-grails.project.fork = [
-        test:    [*: defaultVMSettings, daemon:      true],
-        run:     [*: defaultVMSettings, forkReserve: false],
-        war:     [*: defaultVMSettings, forkReserve: false],
-        console: defaultVMSettings
-]
-
-grails.project.repos.default = 'repo.thehyve.nl-snapshots'
-grails.project.repos."${grails.project.repos.default}".url = 'https://repo.thehyve.nl/content/repositories/snapshots/'
+grails.project.work.dir = 'target'
 
 grails.project.dependency.resolver = 'maven'
 grails.project.dependency.resolution = {
-    log "warn"
-    legacyResolve false
 
-    inherits('global') {}
+	inherits 'global'
+	log 'warn'
+	legacyResolve false
 
-    repositories {
-        //  grailsPlugins()
-        // grailsHome()
-        grailsCentral()
+	repositories {
+		mavenLocal() // Note: use 'grails maven-install' to install required plugins locally
+		grailsCentral()
+		mavenCentral()
+		mavenRepo 'https://repo.transmartfoundation.org/content/repositories/public/'
+	}
 
-        mavenLocal()
-        mavenCentral()
+	dependencies {
+		compile 'org.transmartproject:transmart-core-api:16.2'
+	}
 
-        mavenRepo 'https://repo.transmartfoundation.org/content/repositories/public/'
-        mavenRepo 'https://repo.thehyve.nl/content/repositories/public/'
-    }
+	plugins {
+		compile ':hibernate:3.6.10.19'
 
-    dependencies {
-        compile('org.transmartproject:transmart-core-api:16.2-SNAPSHOT')
-        compile group: 'com.google.guava', name: 'guava', version: '14.0.1'
-
-        runtime('org.postgresql:postgresql:9.3-1100-jdbc41') {
-            transitive = false
-            export     = false
-        }
-    }
-
-    plugins {
-        build ':tomcat:7.0.54'
-        build ':release:3.0.1', ':rest-client-builder:2.1.1', {
-            export = false
-        }
-
-        compile(':db-reverse-engineer:0.5') {
-            export = false
-        }
-
-        runtime ':hibernate:3.6.10.16'
-    }
+		build ':release:3.1.2', ':rest-client-builder:2.1.1', {
+			export = false
+		}
+	}
 }
