c09befdf6020b68e523eef78bcf9a0340cda6a22
Burt Beckwith <burt@burtbeckwith.com>
2018-03-12 08:07:28 -0400

    added report() overload that takes username, event, and eventmessage, defaulting username to the value from the auth principal

--- grails-app/services/org/transmartproject/db/log/AccessLogService.groovy
+++ grails-app/services/org/transmartproject/db/log/AccessLogService.groovy
@@ -4,35 +4,42 @@ import grails.transaction.Transactional
 import org.transmartproject.core.log.AccessLogEntryResource
 import org.transmartproject.core.users.User
 
-@Transactional
 class AccessLogService implements AccessLogEntryResource {
 
-    @Override
-    AccessLogEntry report(Map<String, Object> additionalParams = [:], User user, String event) {
-        new AccessLogEntry(
-                username:       user?.username,
-                event:          event,
-                eventMessage:   additionalParams?.eventMessage,
-                requestURL:     additionalParams?.requestURL,
-                accessTime:     additionalParams?.accessTime ?: new Date(),
-        ).save()
-    }
+	def springSecurityService
 
-    @Override
-    List<AccessLogEntry> listEvents(Map<String, Object> paginationParams = [:], Date startDate, Date endDate) {
-        AccessLogEntry.createCriteria().list(
-                max:    paginationParams?.max,
-                offset: paginationParams?.offset,
-                sort:   paginationParams?.sort,
-                order:  paginationParams?.order) {
+	@Transactional
+	AccessLogEntry report(Map<String, Object> additionalParams = [:], User user, String event) {
+		new AccessLogEntry(
+				username:     user?.username,
+				event:        event,
+				eventMessage: additionalParams?.eventMessage,
+				requestURL:   additionalParams?.requestURL,
+				accessTime:   additionalParams?.accessTime ?: new Date(),
+		).save()
+	}
 
-            if (startDate && endDate) {
-                between 'accessTime', startDate, endDate
-            } else if (startDate) {
-                gte 'accessTime', startDate
-            } else if (endDate) {
-                lte 'accessTime', endDate
-            }
-        }
-    }
+	@Transactional
+	AccessLogEntry report(String username = springSecurityService.principal.username, String event, String message) {
+		new AccessLogEntry(username: username, event: event, eventMessage: message, accessTime: new Date()).save()
+	}
+
+	List<AccessLogEntry> listEvents(Map<String, Object> paginationParams = [:], Date startDate, Date endDate) {
+		AccessLogEntry.createCriteria().list(
+				max:    paginationParams?.max,
+				offset: paginationParams?.offset,
+				sort:   paginationParams?.sort,
+				order:  paginationParams?.order) {
+
+			if (startDate && endDate) {
+				between 'accessTime', startDate, endDate
+			}
+			else if (startDate) {
+				gte 'accessTime', startDate
+			}
+			else if (endDate) {
+				lte 'accessTime', endDate
+			}
+		}
+	}
 }
